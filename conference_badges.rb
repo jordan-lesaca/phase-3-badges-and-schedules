1206661e47fe54eac350fee37872a91d330f901a
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-10-14 22:53:07 -0700

    commit tree cdc220de7e1ce08f81fceb7708bc2d1b432c6b83

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
+require 'pry'
+
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.map{|s| "Hello, my name is #{s}."}
+end
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1){|s,r| 
+    "Hello, #{s}! You'll be assigned to room #{r}!"}
+end
+
+def printer(speaker)
+    batch_badge_creator(speaker).each{|s| puts s }
+    assign_rooms(speaker).each{|dir| puts dir}
+end
